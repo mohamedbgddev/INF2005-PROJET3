1e05fd71a5bc79bad35ff75e9f6d370ea3a03fb6
Bingo <Bingo@172.29.76.174>
2017-12-14 14:00:28 -0500

    Added: validation section identification

--- js/validation.js
+++ js/validation.js
@@ -1,14 +1,18 @@
-function nomFamilleNaissanceValidation() {
-	var nomfamilleNaissance = document.getElementById("Nom-de-famille-a-la-naissance"); 
+/*
+	Fonction de validations Section indentification
+*/
 
-	if(nomfamilleNaissance.value == "") {  
+function nomFamilleNaissanceValidation() {
+	var valeur = document.getElementById("Nom-de-famille-a-la-naissance"); 
+
+	if(valeur.value == "") {  
 		document.getElementById("valider-Nom-de-famille-a-la-naissance").style.display = "block";
-		nomfamilleNaissance.parentNode.className = "form-group has-error has-feedback"; 
+		valeur.classList.add('has-error', 'has-feedback');
 		document.getElementById('valider-Nom-de-famille-a-la-naissance').innerHTML = 'Nom de famille a la naissance requis !';
 		return false; 
 	} else { 
 		document.getElementById("valider-Nom-de-famille-a-la-naissance").style.display = "none"; 
-		nomfamilleNaissance.parentNode.className = "form-group has-success has-feedback";  
+		valeur.classList.add('has-success', 'has-feedback');
 		document.getElementById('valider-Nom-de-famille-a-la-naissance').innerHTML = '';
 		return true; 
 	}
@@ -20,12 +24,16 @@ function dateDeNaissanceValidation() {
 
 	if(valeur.value == "") {  
 		document.getElementById("valider-Date-de-naissance").style.display = "block";
-		valeur.parentNode.className = "form-group has-error has-feedback"; 
+		valeur.classList.add('has-error', 'has-feedback');
 		document.getElementById('valider-Date-de-naissance').innerHTML = ' Date de naissance requise !';
 		return false; 
-	} else { 
+	}else if (!validerDateNaissance(valeur.value)) {
+		document.getElementById("valider-Date-de-naissance").style.display = "block";
+		valeur.classList.add('has-error', 'has-feedback');
+		document.getElementById('valider-Date-de-naissance').innerHTML = 'Format de date invalide !';
+	}else { 
 		document.getElementById("valider-Date-de-naissance").style.display = "none"; 
-		valeur.parentNode.className = "form-group has-success has-feedback";  
+		valeur.classList.add('has-success', 'has-feedback');
 		document.getElementById('valider-Date-de-naissance').innerHTML = '';
 		return true; 
 	}
@@ -37,13 +45,35 @@ function prenomUsuelValidation(){
 
 	if(valeur.value == "") {  
 		document.getElementById("valider-Prenom-usuel").style.display = "block";
-		valeur.parentNode.className = "form-group has-error has-feedback"; 
+		valeur.classList.add('has-error', 'has-feedback');
 		document.getElementById('valider-Prenom-usuel').innerHTML = 'Prenom usuel est requis !';
 		return false; 
 	} else { 
 		document.getElementById("valider-Prenom-usuel").style.display = "none"; 
-		valeur.parentNode.className = "form-group has-success has-feedback";  
+		valeur.classList.add('has-success', 'has-feedback');
 		document.getElementById('valider-Prenom-usuel').innerHTML = '';
+		return true; 
+	}
+}
+
+
+function courrielCandidatValidation() {
+	var valeur = document.getElementById("Courriel-du-candidat"); 
+
+	if(valeur.value == "") {  
+		document.getElementById("valider-Courriel-du-candidat").style.display = "block";
+		valeur.classList.add('has-error', 'has-feedback');
+		document.getElementById('valider-Courriel-du-candidat').innerHTML = ' Le corriel du canddidat est requis !';
+		return false; 
+	} else if(!validerCourriel(valeur.value)) { 
+		document.getElementById("valider-Courriel-du-candidat").style.display = "block";
+		valeur.classList.add('has-warning', 'has-feedback');
+		document.getElementById("valider-Courriel-du-candidat").innerHTML = "Format du courriel est incorrect!"; 
+		return false; 
+	} else { 
+		document.getElementById("valider-Courriel-du-candidat").style.display = "none"; 
+		valeur.classList.add('has-success', 'has-feedback'); 
+		document.getElementById('valider-Courriel-du-candidat').innerHTML = '';
 		return true; 
 	}
 }
@@ -51,3 +81,19 @@ function prenomUsuelValidation(){
 
 
 
+
+
+
+/*
+Fonction de validations de formatage
+*/
+
+function validerDateNaissance(dateNaissance) { 
+	var pattern = ""; 
+	return pattern.test(dateNaissance); 
+}
+
+function validerCourriel(courriel) { 
+	var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
+	return pattern.test(courriel); 
+}
