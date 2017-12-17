89765251a79f6482fd1b1b8fdb9e7a0f3cf54f9d
Julien Labrosse <neiluj@gmail.com>
2017-12-16 19:04:21 -0500

    server validation for second section of second page is done

--- validation.php
+++ validation.php
@@ -8,6 +8,9 @@ define('REGEX_CODE_PERMANENT', '/^[A-Z]{4}\d{8}$/');
 define('REGEX_COURRIEL', '/^[a-zA-Z0-9._-]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/');
 define('REGEX_ASSURANCE_SOCIAL', '/^\d{9}$/');
 define('REGEX_CODE_POSTAL', '/^[A-Z]\d[A-Z][" "]\d[A-Z]\d$/');
+define('REGEX_ANNEE', '/^(19|20)\d\d$/');
+define('REGEX_MOI', '/^(0[1-9]|1[012])$/');
+
 
 
 
@@ -46,11 +49,13 @@ function test(){
   }
 }
 
-function verifierChampsVide($listeChamps){
+function verifierChampsVide($listeChamps, $logger){
   $champsPresents = true;
   foreach ($listeChamps as $champs) {
     if(empty($_POST[$champs])){
-      logger("ERREUR - Le champ ".$champs." est obligatoire.");
+      if($logger){
+        logger("ERREUR - Le champ ".$champs." est obligatoire.");
+      }
       $champsPresents = false;
     }
   }
@@ -77,7 +82,7 @@ function validerSectionIdentification(){
   //tout les champs obligatoires sont pleins
   $champsObligatoires = array("Nom_de_famille_a_la_naissance", "Date_de_naissance", "Prenom_usuel", "radio-sexe", "Code_permanent_ministere", "radio_citoyennete", "Lieu_de_naissance",
     "radio-statut-canada", "radio-langue-usage", "radio-langue-maternelle", "Numéro_civique_Type_et_nom_de_la_rue_Direction_de_rue");
-  if(!verifierChampsVide($champsObligatoires)){
+  if(!verifierChampsVide($champsObligatoires, true)){
     return false;
   }
 
@@ -139,19 +144,21 @@ function validerSectionIdentification(){
 function validerSectionProgrammes(){
   //tout les champs obligatoires sont pleins
   $champsObligatoires = array("radio-trimestre", "trimestre_annee", "premier_choix_titre", "radio-temp-1", "premier_choix_code", "radio-type-programme-1");
-  if(!verifierChampsVide($champsObligatoires)){
+  if(!verifierChampsVide($champsObligatoires, true)){
     return false;
   }
 //présence au québec??
 
   //deuxieme choix, soit tout les champs vide ou tout les champs remplis
-  if( !(empty($_POST["deuxieme_choix_titre"]) === empty($_POST["deuxieme_choix_code"]) === empty($_POST["radio-temp-2"]) === empty($_POST["radio-type-programme-2"])) ){
+  $listeDeuxieme = array(empty($_POST["deuxieme_choix_titre"]), empty($_POST["deuxieme_choix_code"]), empty($_POST["radio-temp-2"]), empty($_POST["radio-type-programme-2"]));
+  if(!(arrayBooleanUniqueCount($listeDeuxieme))){
     logger("ERREUR - deuxieme choix invalide.");
     return false;
   }
 
   //troisieme choix, soit tout les champs vide ou tout les champs remplis
-  if( !(empty($_POST["troisieme_choix_titre"]) === empty($_POST["troisieme_choix_code"]) === empty($_POST["radio-temp-3"]) === empty($_POST["radio-type-programme-3"])) ){
+  $listeTroisieme = array(empty($_POST["troisieme_choix_titre"]), empty($_POST["troisieme_choix_code"]), empty($_POST["radio-temp-3"]), empty($_POST["radio-type-programme-3"]));
+  if( !(arrayBooleanUniqueCount($listeTroisieme)) ){
     logger("ERREUR - troisieme choix invalide.");
     return false;
   }
@@ -159,17 +166,89 @@ function validerSectionProgrammes(){
   return true;
 }
 
-/*
-function validerSectionEtudeSC(){
+function comparerEtValiderDates($debut, $fin){
+  $debutValide = empty($_POST[$debut]) ? false : preg_match(REGEX_ANNEE, $_POST[$debut]);
+  $finValide = empty($_POST[$fin]) ? false : preg_match(REGEX_ANNEE, $_POST[$fin]);
+  if($finValide && $debutValide){
+    $diff = $_POST[$fin] - $_POST[$debut];
+    $diffValide = $diffAnneeHQ > 0 ? true : false;
+    return $diffValide;
+  }
+  return false;
+}
 
-  logger("INFO - section etude secondaires et collegiales valide.");
+function validerSectionEtudeSC(){
+  //aucun champs obligatoires. Si un diplome est specifie tout ses champs doivent etre remplis.
+
+  //derniere annee du secondaire et dates
+  if(!empty($_POST["derniere_annee_secondaire"])){
+    if(!comparerEtValiderDates("de_annee_derniere_annee_secondaire", "a_annee_derniere_annee_secondaire")){
+      logger("ERREUR - Les dates de periode de fréquentation ne sont pas valides.");
+      return false;
+    }
+  }
+  //diplome d'etude secondaire ou collegiales a l'exterieur du QC
+  $champsDiplomeHQ = array(empty($_POST["diplome_sec_col_hors_quebec"]), empty($_POST["discipline_specialisation_hors_quebec"]), empty($_POST["institution_hors_quebec"]),
+      empty($_POST["pays_hors_quebec"]), empty($_POST["annee_obtention_diplome_hors_quebec"]), empty($_POST["mois_obtention_diplome_hors_quebec"]),
+      empty($_POST["de_annee_diplome_hors_quebec"]), empty($_POST["a_annee_diplome_hors_quebec"]));
+
+  if(arrayBooleanUniqueCount($champsDiplomeHQ) && !empty($_POST["diplome_sec_col_hors_quebec"])){
+    $nomDiplome = empty($_POST["diplome_sec_col_hors_quebec"]) ? false : preg_match(REGEX_TAILLE_CHAMPS, $_POST["diplome_sec_col_hors_quebec"]);
+    $disciplineSpecialisationHQValide = empty($_POST["discipline_specialisation_hors_quebec"]) ? false : preg_match(REGEX_TAILLE_CHAMPS, $_POST["discipline_specialisation_hors_quebec"]);
+    $institutionHQValide = empty($_POST["institution_hors_quebec"]) ? false : preg_match(REGEX_TAILLE_CHAMPS, $_POST["institution_hors_quebec"]);
+    $paysValide = empty($_POST["pays_hors_quebec"]) ? false : preg_match(REGEX_TAILLE_CHAMPS, $_POST["pays_hors_quebec"]);
+    $anneeObtentionHQ = empty($_POST["annee_obtention_diplome_hors_quebec"]) ? false : preg_match(REGEX_ANNEE, $_POST["annee_obtention_diplome_hors_quebec"]);
+    $moiObtentionHQ = empty($_POST["mois_obtention_diplome_hors_quebec"]) ? false : preg_match(REGEX_ANNEE, $_POST["mois_obtention_diplome_hors_quebec"]);
+    $anneeHQValide = comparerEtValiderDates("de_annee_diplome_hors_quebec", "a_annee_diplome_hors_quebec");
+    if(!($nomDiplome && $disciplineSpecialisationHQValide && $institutionHQValide && $paysValide && $anneeObtentionHQ && $moiObtentionHQ && $anneeHQValide)){
+      logger("ERREUR - section etutde secondaires et collegiales hors Quebec invalide.");
+      return false;
+    }
+  } else if (!arrayBooleanUniqueCount($champsDiplomeHQ)){
+    logger("ERREUR - section etutde secondaires et collegiales hors Quebec invalide.");
+    return false;
+  }
+
+  //DEC ou autre
+  $champsDiplomeQC = array(empty($_POST["radio-DEC_ou_autre"]), empty($_POST["diplome_sec_col_dans_quebec"]), empty($_POST["discipline_specialisation_dans_quebec"]),
+      empty($_POST["institution_dans_quebec"]), empty($_POST["de_annee_diplome_dans_quebec"]), empty($_POST["a_annee_diplome_dans_quebec"]),
+      empty($_POST["radio-obtention-diplome-page-2"]), empty($_POST["mois_obtention_diplome_dans_quebec"]), empty($_POST["a_annee_diplome_dans_quebec"]));
+
+  if(arrayBooleanUniqueCount($champsDiplomeQC) && !empty($_POST["diplome_sec_col_dans_quebec"])){
+    $nomDiplomeQC = empty($_POST["diplome_sec_col_dans_quebec"]) ? false : preg_match(REGEX_TAILLE_CHAMPS, $_POST["diplome_sec_col_dans_quebec"]);
+    $disciplineSpecialisationQCValide = empty($_POST["discipline_specialisation_dans_quebec"]) ? false : preg_match(REGEX_TAILLE_CHAMPS, $_POST["discipline_specialisation_dans_quebec"]);
+    $institutionQCValide = empty($_POST["institution_dans_quebec"]) ? false : preg_match(REGEX_TAILLE_CHAMPS, $_POST["institution_dans_quebec"]);
+    $anneeObtentionQC = empty($_POST["de_annee_diplome_dans_quebec"]) ? false : preg_match(REGEX_ANNEE, $_POST["de_annee_diplome_dans_quebec"]);
+    $moiObtentionQC = empty($_POST["mois_obtention_diplome_dans_quebec"]) ? false : preg_match(REGEX_ANNEE, $_POST["mois_obtention_diplome_dans_quebec"]);
+    $anneeQCValide = comparerEtValiderDates("a_annee_diplome_dans_quebec", "de_annee_diplome_dans_quebec");
+    if(!($nomDiplome && $disciplineSpecialisationQCValide && $institutionQCValide && $paysValide && $anneeObtentionQC && $moiObtentionQC && $anneeQCValide)){
+      logger("ERREUR - section etutde secondaires et collegiales dans le Quebec invalide.");
+      return false;
+    }
+  } else if (!arrayBooleanUniqueCount($champsDiplomeQC)){
+    logger("ERREUR - section etutde secondaires et collegiales dans le Quebec invalide.");
+    return false;
+  }
+
+  logger("INFO - section etude secondaires et collegiales dans le Quebec valide.");
   return true;
-}*/
+}
+
+//fonction qui prend une liste de booleans en parametre et qui retourne vrai si
+//tout les membres on la meme valeur, faux sinon.
+function arrayBooleanUniqueCount($array){
+  $unique = array_unique($array);
+  $count = count($unique);
+  if($count === 1){
+    return true;
+  }
+  return false;
+}
 
 if ($_POST) {
   $identificationValide = validerSectionIdentification();
   $programmesValide = validerSectionProgrammes();
-  //$etudeSecondairesEtCollegialesValide = validerSectionEtudeSC();
+  $etudeSecondairesEtCollegialesValide = validerSectionEtudeSC();
   //test();
   /*foreach ($_POST as $param_name => $param_val) {
     logger( "Param: $param_name - Value: $param_val");
